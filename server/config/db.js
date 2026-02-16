36371b2bfc9835adc8247e8460e94687977f9d59
viraj-gujarati77 <viraj.trionn@gmail.com>
2026-02-16 18:05:32 +0530

    Add MongoDB connection function to db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ Database Connection Failed:", err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
 
