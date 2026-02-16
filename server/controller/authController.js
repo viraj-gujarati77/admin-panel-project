9c6304341e8e516fa657315cfe2121bc0285a7bf
viraj-gujarati77 <viraj.trionn@gmail.com>
2026-02-16 18:06:37 +0530

    Update authController.js

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,1 +1,12 @@
+const User = require("../models/User");
+const { generateToken } = require("../services/jwtService");
 
+exports.login = async (req, res) => {
+  // ... (user validation logic with database)
+  if (userIsValid) {
+    const token = generateToken({ id: user._id, email: user.email }); //
+    res.json({ message: "Login successful!", token });
+  } else {
+    res.status(401).json({ message: "Invalid credentials" });
+  }
+};
